bd378468a22ceff1331e17133f0918dc9cc83190
Son1c-sound <109432830+Son1c-sound@users.noreply.github.com>
2024-07-22 11:38:30 -0400

    Update undersection.svelte

--- src/lib/Hero/UnderSection/undersection.svelte
+++ src/lib/Hero/UnderSection/undersection.svelte
@@ -7,8 +7,8 @@
     <div class='w-full md:w-1/2 flex flex-col md:flex-row justify-center items-center'>
         <img class='mx-auto w-full md:w-2/4  mb-6 md:mb-0 md:mr-6' src="https://gratisography.com/wp-content/uploads/2024/01/gratisography-cyber-kitty-800x525.jpg" alt="Cyber Kitty">
         <div class='text-center md:text-left'>
-            <p class='text-lg font-bold mb-2'>BASIX COLLECTION</p>
-            <p class='text-sm mb-3'>Odd Sox Basix are more than just an everyday sock. Providing extreme comfort and unmatched quality, Basix are built for top performance at the most affordable level. Made for whoever you are and whatever you do, we guarantee you just found your new favorite socks.</p>
+            <p class='text-lg font-bold mb-2'>Sock Vibes</p>
+            <p class='text-sm mb-3'>Embrace comfort and style with Sock Vibes, where we offer more than just socks. Our diverse collection includes cozy slippers, playful toys, and other lifestyle essentials that add a touch of joy to your everyday life. From vibrant patterns to delightful designs, Sock Vibes has something for everyone, making every moment a little more special.</p>
             <Button  class='bg-white text-gray-900 hover:text-gray-100 mx-auto'>contact support</Button>
         </div>
     </div>
