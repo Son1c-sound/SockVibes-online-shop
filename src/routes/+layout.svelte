268a9fbf1e01eccc3c1f78f568f48d1b95f9fe93
TechSon1c <makaradze98.s@gmail.com>
2024-07-27 04:13:03 -0400

    analyics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,9 @@
     import FooterSection from '../lib/Footer/footersection.svelte'
   import Navbar from "$lib/Navbar/navbar.svelte";
     
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+    inject({ mode: dev ? 'development' : 'production' });  
 
 import "../app.css";
 
