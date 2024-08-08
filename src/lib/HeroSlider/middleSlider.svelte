169ef6c6b6a261c5c2e993a0417e2b9a6d88cf57
TechSon1c <makaradze98.s@gmail.com>
2024-08-08 12:20:15 -0400

    test

--- src/lib/HeroSlider/middleSlider.svelte
+++ src/lib/HeroSlider/middleSlider.svelte
@@ -1,44 +1,39 @@
 <script lang="ts">
-   import Button from "$lib/components/ui/button/button.svelte";
-    import * as Card from "$lib/components/ui/card/";
-    import * as Carousel from "$lib/components/ui/carousel/";
-    import supabase from "$lib/db";
-    
-    import Autoplay from "embla-carousel-autoplay";
-    import { goto } from "$app/navigation";
-    import { type CarouselAPI } from "$lib/components/ui/carousel/context.js";
-    import { Progress } from "$lib/components/ui/progress/index.js";
-    import Contentload from "$lib/loading/contentload.svelte";
+  import Button from "$lib/components/ui/button/button.svelte";
+  import * as Card from "$lib/components/ui/card/";
+  import * as Carousel from "$lib/components/ui/carousel/";
+  import supabase from "$lib/db";
+
+  import Autoplay from "embla-carousel-autoplay";
+  import { goto } from "$app/navigation";
+  import { type CarouselAPI } from "$lib/components/ui/carousel/context.js";
+  import { Progress } from "$lib/components/ui/progress/index.js";
+  import Contentload from "$lib/loading/contentload.svelte";
   import { onMount } from "svelte";
 
-  
-    function navigateToSocks(socksId: any) {
-        goto(`CheckOut/products/Socks/${socksId}`);
-    }
+  function navigateToSocks(socksId: any) {
+    goto(`CheckOut/products/Socks/${socksId}`);
+  }
 
-  
- 
-   
   const plugin = Autoplay({ delay: 4000, stopOnInteraction: true });
 
+  let api: CarouselAPI;
+  let count = 0;
+  let current = 0;
 
-    let api: CarouselAPI;
-    let count = 0;
-    let current = 0;
-  
-    $: if (api) {
-      count = api.scrollSnapList().length;
+  $: if (api) {
+    count = api.scrollSnapList().length;
+    current = api.selectedScrollSnap() + 1;
+    api.on("select", () => {
       current = api.selectedScrollSnap() + 1;
-      api.on("select", () => {
-        current = api.selectedScrollSnap() + 1;
-      });
-    }
+    });
+  }
 
-    // loading data from supabase
-    let errorMessage:any = ''
-    let items:any[] = []
-    let loading = true;
-    async function loadItems() {
+  // loading data from supabase
+  let errorMessage: any = "";
+  let items: any[] = [];
+  let loading = true;
+  async function loadItems() {
     try {
       const { data, error } = await supabase.from("popularItems").select("*");
 
@@ -51,67 +46,64 @@
     } catch (error) {
       errorMessage = `Unexpected error: ${error}`;
     } finally {
-      loading = false; 
+      loading = false;
     }
-}
+  }
 
-
-  
-onMount(() => {
+  onMount(() => {
     loadItems();
   });
-
-  </script>
+</script>
 
 {#if loading}
-
-<Contentload></Contentload>
-
+  <Contentload></Contentload>
 {:else}
-     
-   <Carousel.Root bind:api  plugins={[plugin]}
-
-   on:mousenter={plugin.stop}
-   on:mouseleave={plugin.reset} 
-   class="w-3/4 mx-auto my-5">
-   <h1 class="text-3xl  text-center mb-10 bg-gradient-to-r from-gray-700 to-blue-500 text-transparent bg-clip-text  italic">Popular  Items in store</h1>
-
-  
+  <Carousel.Root
+    bind:api
+    plugins={[plugin]}
+    on:mousenter={plugin.stop}
+    on:mouseleave={plugin.reset}
+    class="w-3/4 mx-auto my-5"
+  >
+    <h1
+      class="text-3xl text-center mb-10 bg-gradient-to-r from-gray-700 to-blue-500 text-transparent bg-clip-text italic"
+    >
+      Popular Items in store
+    </h1>
+ 
     <Carousel.Content class="-ml-1">
       {#each items as slide}
-        <Carousel.Item class="pl-6 md:basis-1/2 lg:basis-1/5">
+        <Carousel.Item class="pl-4 md:basis-1/2 lg:basis-1/4">
           <button on:click={() => navigateToSocks(slide.id)}>
                 
             <Card.Root class='border-none shadow-none bg-transparent'>
               <Card.Content>
               </Card.Content>
-
-                <div class=" flex flex-col justify-center items-center">
+ 
+                <div class=" h-96 flex flex-col justify-center items-center">
                   <img src={slide.img} alt="" class="h-full  object-cover">
                   <p class="text-center  mt-2 text-xl text-gray-900">{slide.name}</p>
                   <p class="text-center  mt-2 text-xl text-gray-900">{slide.price}$</p>
                 </div>
   
-
+ 
             </Card.Root>
           </button>
         </Carousel.Item>
       {/each}
-
+ 
  
     </Carousel.Content>
 
-
-
     <Carousel.Previous />
-    
+
     <Carousel.Next />
-    <Progress value={current} max={5} class='h-1 my-10' />
+    <Progress value={current} max={5} class="h-1 my-10" />
   </Carousel.Root>
 {/if}
- 
-  <style>
-   * {
-        font-family:  Arial, Helvetica, sans-serif;
-    }
-  </style>
+
+<style>
+  * {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+</style>
